3432fd398a89cbc40bba50f1d5368a18ea8e556f
lukaskory <69061294+lukaskory@users.noreply.github.com>
2020-07-31 16:34:33 -0700

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,43 +1,9 @@
-print("Welcome to my Calculator")
+#Addition Calculator made by lukas
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("Hello I am your personal calculator R@t")
+a = input("Enter your first number ")
+b = input("Enter your second number ")
+c = float(a) * float(b)
+print("Your answer is ")
+print(c)
